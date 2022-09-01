7f9b0d463273f3d4519530945fa9c7e3e670b488
Andoni Mihai <andonimihai@gmail.com>
2022-09-01 21:16:53 +0300

    add nvim-treesitter-context

--- nvim/lua/code_am/packer.lua
+++ nvim/lua/code_am/packer.lua
@@ -20,6 +20,7 @@ return require('packer').startup(function(use)
     'nvim-treesitter/nvim-treesitter',
     run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
   }
+  use 'nvim-treesitter/nvim-treesitter-context'
 
   use { 'glepnir/dashboard-nvim' }
 
